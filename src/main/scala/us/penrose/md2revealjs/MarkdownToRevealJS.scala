0dd074ab1f5f7aeff40d8dbe6213b83a18556d81
Stu Penrose <stu@penrose.us>
2020-01-29 13:10:15 -0800

    Now presentations are nested

--- src/main/scala/us/penrose/md2revealjs/MarkdownToRevealJS.scala
+++ src/main/scala/us/penrose/md2revealjs/MarkdownToRevealJS.scala
@@ -29,7 +29,7 @@ object MarkdownToRevealJS extends App {
   }
   val inputPath = new FilesystemPath(args(0))
   val outputPath = new FilesystemPath(args(1))
-  val title = args(2)
+  val title = params.get("title").flatten.getOrElse("A Presentation")
   val headingLevel = params.get("slide-heading-level").flatten.map(_.toInt).getOrElse(1)
   
   var lastModified = 0L;
@@ -47,94 +47,120 @@ object MarkdownToRevealJS extends App {
   
   def render(inputPath:FilesystemPath, outputPath:FilesystemPath, title:String){
     import org.commonmark.parser.Parser
-    
-    
+
     val template = IOUtils.toString(getClass.getResourceAsStream("template.html"))
-    
-  //  println("Reading " + inputPath)
+
     val markdown = FileUtils.readFileToString(inputPath, Charset.forName("ASCII"))
     
     val parser = Parser.builder().build();
     
     val document = parser.parse(markdown);
-    
-    def reduceHeadings(n:Node){
-      n match {
-        case h:Heading => h.setLevel(h.getLevel + headingLevel)
-        case _ => 
-      }
-      streamNodes(n).foreach(reduceHeadings)
-    }
-    reduceHeadings(document)
-    
-    
-    
+
     val nodes = streamNodes(document).toList
-    
-  //  println(nodes.size + " nodes")
-    
-    
-    val slides = nodes.foldLeft(Seq[Slide]()){(slides, n)=> 
+
+    case class Accum(currentSection:Section, root:Section){
+      def withSiblingSection(s:Section) = {
+        println("adding sibling " + s.name + " after " + currentSection.name)
+        val updatedTree = root.withSiblingAdded(currentSection, s)
+        copy(
+          currentSection = s,
+          root = updatedTree)
+      }
+
+      def withNestedSection(s:Section) = {
+        println("adding nested " + s.name + " under " + currentSection.name)
+        withUpdatedCurrentSection(currentSection.copy(subsections = currentSection.subsections :+ s))
+
+        val updatedCurrentSection = currentSection.copy(subsections = currentSection.subsections :+ s)
+        copy(
+          currentSection = s,
+          root = root.withUpdate(currentSection, updatedCurrentSection))
+      }
+
+      def withUpdatedCurrentSection(updatedSection:Section) = {
+        copy(
+          currentSection = updatedSection,
+          root = root.withUpdate(currentSection, updatedSection))
+      }
+
+      def withNewAtLevel(level:Int, s:Section) = {
+
+        copy(
+          currentSection = s,
+          root = root.withNewAtLevel(level, s))
+      }
+    }
+
+    trait DocumentSection
+    case class Section(id:String, name:String, h:Heading, body:List[Node] = List(), level:Int, subsections:Seq[Section] = Seq()){
+      def getSectionByName(name:String) : Option[Section] = {
+        if(this.name == name) Some(this)
+        else subsections.map{s=> s.getSectionByName(name)}.flatten.headOption
+      }
+      def withSiblingAdded(target:Section, newSibling:Section): Section ={
+        if(subsections.contains(target)){
+          copy(subsections = subsections :+ newSibling)
+        }else{
+          copy(subsections = subsections.map(_.withSiblingAdded(target, newSibling)))
+        }
+      }
+
+      def withNewAtLevel(level:Int, s:Section) : Section = {
+        if(this.level + 1 == level){
+          copy(subsections = subsections :+ s)
+        }else{
+          // add it to the last
+          val leading = subsections.take(subsections.size-1)
+          val updatedLast = subsections.lastOption.map(_.withNewAtLevel(level, s))
+
+          copy(subsections = leading ++ updatedLast)
+        }
+      }
+
+      def withUpdate(target:Section, update:Section): Section ={
+        if(target == this){
+          update
+        }else{
+          copy(subsections = subsections.map(_.withUpdate(target, update)))
+        }
+      }
+    }
+    def mkId(h:Heading) = {
+      streamNodes(h).map(toText).mkString.filter{char=> char.isLetter || char.isDigit || Set('-', '.', '_').contains(char)}
+    }
+
+    val emptyRoot = Section(id = "root", h = null, name = title , level = 0)
+    val root = nodes.foldLeft(Accum(currentSection = emptyRoot, root = emptyRoot)){(accum, n)=>
+
+      val currentSection = accum.currentSection
+
       n match {
         case h:Heading => {
-          slides :+ Slide(
-                        id = streamNodes(h).map(toText).mkString.filter{char=> char.isLetter || char.isDigit || Set('-', '.', '_').contains(char)},
-                        h = h, 
-                        body = List())
+          val newSection = Section(id = mkId(h),
+            h = h,
+            body = List(),
+            name = streamNodes(h).map(toText).mkString, level = h.getLevel)
+
+          accum.withNewAtLevel(h.getLevel, newSection)
         }
         case n:Node => {
-          slides.lastOption match {
-            case None => slides
-            case Some(s) => {
-              val all = slides.slice(0, slides.size-1)
-              all :+ s.copy(body = s.body :+ n)
-            }
-          }
+
+          val updatedSection = currentSection.copy(body = currentSection.body :+ n)
+
+          accum.withUpdatedCurrentSection(updatedSection)
         }
       }
-    }.filterNot{slide=> textSubContent(slide.h).trim().toLowerCase() == "hidden"}
-    
-    
-    val expandedSlides = {
-      val slidesById = groupSequential(slides.toList)({slide=> slide.id})
-      
-      slidesById.flatMap{case (id, group)=>
-        if(group.size>1){
-          group.zipWithIndex.map{case (slide, idx) =>
-            val num = idx + 1
-            slide.copy(id = if(num>1) slide.id + num else slide.id)
-          }
-        }else {
-          group
-        }
-      }
-      
-    }//.flatMap(SlideReducer.breakApartIfTooLarge(_, 5))
-    
-    def nestedNodeCount(n:Node):Int = {
-      streamNodes(n).foldLeft(0){(accum, child)=>
-        accum + 1 + nestedNodeCount(child)
-      }
+    }.root
+
+
+    def printS(s:Section, whitespace:String = ""): Unit ={
+      println(whitespace + "Section: " + s.name + " (level " + s.level + ")  -  " + s.subsections.size + " subsections")
+
+      s.subsections.foreach(printS(_, whitespace + " "))
     }
-    
-    val outline = slides.flatMap{slide=>
-      slide.h match {
-        case h:Heading => {
-          
-          val text = textSubContent(h)
-          
-          h.getLevel match {
-            case 2 => List(text)
-            case _ => List()
-          }
-          
-        }
-        case _:Node => List()
-      }
-    }
-    
-    
-    
+
+    printS(root, "")
+
     val slideTemplate = """
   				<section id="ID_GOES_HERE">
   					<h3>TITLE_GOES_HERE</h3>
@@ -165,29 +191,42 @@ object MarkdownToRevealJS extends App {
       case ul:BulletList => toSpecialBullets(n)
       case n:Node => toHtml(n)
     }
-    
-    val slidesHtml = expandedSlides.zipWithIndex.map({case (slide, idx)=>
-  //    println("HEADER:" + toHtml(slide.h))
-      val bodyHtml = slide.body.map(toSpecialHtml).mkString("\n")
-      
-      slideTemplate
-        .replaceAllLiterally("ID_GOES_HERE", slide.id)
-        .replaceAllLiterally("TITLE_GOES_HERE", toHtml(slide.h))
-        .replaceAllLiterally("BODY_GOES_HERE", bodyHtml)
-        
-    }).mkString
-    
-    val outlineSlideHtml = outline.map({name=>
-  //    println("OUTLINE: " + name)
-       s"""<li>$name</li>"""
-    }).mkString("""<section id="Outline"><h1>Outline</h1><ul>""", "\n", "</ul></section>")
+
+    def renderSection(section:Section, outlineNameOverride:Option[String] = None):String = {
+
+      val outline = if(section.subsections.size <= 1) "" else {
+        val outline:Seq[String] = section.subsections.map(_.name)
+        println("Outline for " + section.name + ": " + outline.mkString(", "))
+        outline.map({name=>
+          //    println("OUTLINE: " + name)
+          s"""<li>$name</li>"""
+        }).mkString(s"""<section id="${section.id + "-outline"}"><h2>${outlineNameOverride.getOrElse(section.name)}</h2><ul>""", "\n", "</ul></section>")
+      }
+
+      val slideHtml = if(section.body.isEmpty) "" else {
+        val bodyHtml = section.body.map(toSpecialHtml).mkString("\n")
+
+        slideTemplate
+          .replaceAllLiterally("ID_GOES_HERE", section.id)
+          .replaceAllLiterally("TITLE_GOES_HERE", toHtml(section.h))
+          .replaceAllLiterally("BODY_GOES_HERE", bodyHtml)
+      }
+
+      val subsectionsHtml = section.subsections.map(renderSection(_, None)).mkString("\n\n")
+
+      outline + slideHtml + subsectionsHtml
+    }
+
+    val effectiveRoot = if(root.body.isEmpty && root.subsections.size == 1) root.subsections.head else root
+
+    val bodyHtml = renderSection(effectiveRoot, Some("Outline"))
+
     
     val html = template
-          .replaceAllLiterally("TITLE_GOES_HERE", title)
-          .replaceAllLiterally("SLIDES_GO_HERE", outlineSlideHtml + slidesHtml)
+          .replaceAllLiterally("TITLE_GOES_HERE", effectiveRoot.name)
+          .replaceAllLiterally("SLIDES_GO_HERE",bodyHtml)
           
     FileUtils.writeStringToFile(outputPath, html)
-  //  println("Wrote to " + outputPath)
   }
   
   
